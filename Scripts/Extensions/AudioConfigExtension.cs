942659ebf866d50276ee83cb274c12699c6b40fa
Derek Sliman <lionext.studio@gmail.com>
2025-12-08 21:47:17 +0200

    Fixed audio source access

--- Scripts/Extensions/AudioConfigExtension.cs
+++ Scripts/Extensions/AudioConfigExtension.cs
@@ -8,44 +8,44 @@ namespace TinyServices.Audio.Extensions {
     public static class AudioConfigExtension {
         private static readonly Vector3 _zeroPosition = Vector3.zero;
         
-        public static void Play<T>(this T config) where T : AudioConfig {
-            AudioService.instance.player.Play(config, _zeroPosition);
+        public static AudioSource Play<T>(this T config) where T : AudioConfig {
+            return AudioService.instance.player.Play(config, _zeroPosition);
         }
         
-        public static void Play<T>(this T config, Vector3 position) where T : AudioConfig {
-            AudioService.instance.player.Play(config, position);
+        public static AudioSource Play<T>(this T config, Vector3 position) where T : AudioConfig {
+            return AudioService.instance.player.Play(config, position);
         }
         
-        public static void PlayLoop<T>(this T config, string key) where T : AudioConfig {
-            AudioService.instance.playerLoop.PlayLoop(key, _zeroPosition, config);
+        public static AudioSource PlayLoop<T>(this T config, string key) where T : AudioConfig {
+            return AudioService.instance.playerLoop.PlayLoop(key, _zeroPosition, config);
         }
         
-        public static void PlayLoop<T>(this T config) where T : AudioConfig {
-            AudioService.instance.playerLoop.PlayLoop(config.ToString(), _zeroPosition, config);
+        public static AudioSource PlayLoop<T>(this T config) where T : AudioConfig {
+            return AudioService.instance.playerLoop.PlayLoop(config.ToString(), _zeroPosition, config);
         }
         
-        public static void PlayLoop<T>(this T config, string key, Transform root) where T : AudioConfig {
-            AudioService.instance.playerLoop.PlayLoop(key, root, _zeroPosition, config);
+        public static AudioSource PlayLoop<T>(this T config, string key, Transform root) where T : AudioConfig {
+            return AudioService.instance.playerLoop.PlayLoop(key, root, _zeroPosition, config);
         }
         
-        public static void PlayLoop<T>(this T config, Transform root) where T : AudioConfig {
-            AudioService.instance.playerLoop.PlayLoop(config.ToString(), root, _zeroPosition, config);
+        public static AudioSource PlayLoop<T>(this T config, Transform root) where T : AudioConfig {
+            return AudioService.instance.playerLoop.PlayLoop(config.ToString(), root, _zeroPosition, config);
         }
         
-        public static void PlayLoop<T>(this T config, string key, Vector3 position) where T : AudioConfig {
-            AudioService.instance.playerLoop.PlayLoop(key, position, config);
+        public static AudioSource PlayLoop<T>(this T config, string key, Vector3 position) where T : AudioConfig {
+            return AudioService.instance.playerLoop.PlayLoop(key, position, config);
         }
         
-        public static void PlayLoop<T>(this T config, Vector3 position) where T : AudioConfig {
-            AudioService.instance.playerLoop.PlayLoop(config.ToString(), position, config);
+        public static AudioSource PlayLoop<T>(this T config, Vector3 position) where T : AudioConfig {
+            return AudioService.instance.playerLoop.PlayLoop(config.ToString(), position, config);
         }
         
-        public static void PlayLoop<T>(this T config, string key, Transform root, Vector3 position) where T : AudioConfig {
-            AudioService.instance.playerLoop.PlayLoop(key, root, position, config);
+        public static AudioSource PlayLoop<T>(this T config, string key, Transform root, Vector3 position) where T : AudioConfig {
+            return AudioService.instance.playerLoop.PlayLoop(key, root, position, config);
         }
         
-        public static void PlayLoop<T>(this T config, Transform root, Vector3 position) where T : AudioConfig {
-            AudioService.instance.playerLoop.PlayLoop(config.ToString(), root, position, config);
+        public static AudioSource PlayLoop<T>(this T config, Transform root, Vector3 position) where T : AudioConfig {
+            return AudioService.instance.playerLoop.PlayLoop(config.ToString(), root, position, config);
         }
         
         public static void StopLoop<T>(this T _, string key) where T : AudioConfig {
--- Scripts/Extensions/AudioGroupExtension.cs
+++ Scripts/Extensions/AudioGroupExtension.cs
@@ -7,44 +7,44 @@ using UnityEngine;
 
 namespace TinyServices.Audio.Extensions {
     public static class AudioGroupExtension {
-        public static void Play<T>(this AudioGroup<T> group, T type) where T : Enum {
-            group.GetConfigs()[type].Play();
+        public static AudioSource Play<T>(this AudioGroup<T> group, T type) where T : Enum {
+            return group.GetConfigs()[type].Play();
         }
         
-        public static void Play<T>(this AudioGroup<T> group, T type, Vector3 position) where T : Enum {
-            group.GetConfigs()[type].Play(position);
+        public static AudioSource Play<T>(this AudioGroup<T> group, T type, Vector3 position) where T : Enum {
+            return group.GetConfigs()[type].Play(position);
         }
         
-        public static void PlayLoop<T>(this AudioGroup<T> group, T type, string key) where T : Enum {
-            group.GetConfigs()[type].PlayLoop(key);
+        public static AudioSource PlayLoop<T>(this AudioGroup<T> group, T type, string key) where T : Enum {
+            return group.GetConfigs()[type].PlayLoop(key);
         }
         
-        public static void PlayLoop<T>(this AudioGroup<T> group, T type) where T : Enum {
-            group.GetConfigs()[type].PlayLoop();
+        public static AudioSource PlayLoop<T>(this AudioGroup<T> group, T type) where T : Enum {
+            return group.GetConfigs()[type].PlayLoop();
         }
         
-        public static void PlayLoop<T>(this AudioGroup<T> group, T type, string key, Transform root) where T : Enum {
-            group.GetConfigs()[type].PlayLoop(key, root);
+        public static AudioSource PlayLoop<T>(this AudioGroup<T> group, T type, string key, Transform root) where T : Enum {
+            return group.GetConfigs()[type].PlayLoop(key, root);
         }
         
-        public static void PlayLoop<T>(this AudioGroup<T> group, T type, Transform root) where T : Enum {
-            group.GetConfigs()[type].PlayLoop(root);
+        public static AudioSource PlayLoop<T>(this AudioGroup<T> group, T type, Transform root) where T : Enum {
+            return group.GetConfigs()[type].PlayLoop(root);
         }
         
-        public static void PlayLoop<T>(this AudioGroup<T> group, T type, string key, Vector3 position) where T : Enum {
-            group.GetConfigs()[type].PlayLoop(key, position);
+        public static AudioSource PlayLoop<T>(this AudioGroup<T> group, T type, string key, Vector3 position) where T : Enum {
+            return group.GetConfigs()[type].PlayLoop(key, position);
         }
         
-        public static void PlayLoop<T>(this AudioGroup<T> group, T type, Vector3 position) where T : Enum {
-            group.GetConfigs()[type].PlayLoop(position);
+        public static AudioSource PlayLoop<T>(this AudioGroup<T> group, T type, Vector3 position) where T : Enum {
+            return group.GetConfigs()[type].PlayLoop(position);
         }
         
-        public static void PlayLoop<T>(this AudioGroup<T> group, T type, string key, Transform root, Vector3 position) where T : Enum {
-            group.GetConfigs()[type].PlayLoop(key, root, position);
+        public static AudioSource PlayLoop<T>(this AudioGroup<T> group, T type, string key, Transform root, Vector3 position) where T : Enum {
+            return group.GetConfigs()[type].PlayLoop(key, root, position);
         }
         
-        public static void PlayLoop<T>(this AudioGroup<T> group, T type, Transform root, Vector3 position) where T : Enum {
-            group.GetConfigs()[type].PlayLoop(root, position);
+        public static AudioSource PlayLoop<T>(this AudioGroup<T> group, T type, Transform root, Vector3 position) where T : Enum {
+            return group.GetConfigs()[type].PlayLoop(root, position);
         }
         
         public static void StopLoop<T>(this AudioGroup<T> group, T type, string key) where T : Enum {
